65106abf1161828f8a69e257dd5ab443704501f6
AaronMcClure <aaron.t.mcclure@gmail.com>
2014-10-10 10:55:41 -0400

    test conflict

--- webRoot/init.js
+++ webRoot/init.js
@@ -6,6 +6,7 @@ function init() {
     /*
      * Create the stage
      */
+	 var test = 2;
     stage = new createjs.Stage("demoCanvas");
     startContainer = new createjs.Container();
     menuContainer = new createjs.Container();
