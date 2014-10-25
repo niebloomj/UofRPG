a792ea5f6f8e9c059f703fb8fb541f9e8531ca10
BradleyBeyers <beyers.bradley@gmail.com>
2014-10-25 16:07:49 -0400

    Added stage update, commented out dragger

--- webRoot/ball.js
+++ webRoot/ball.js
@@ -14,16 +14,16 @@ function createGame() {
 	label.textAlign = "center";
 	label.y = -7;
 
-	var dragger = new createjs.Container();
-	dragger.x = dragger.y = 100;
-	dragger.addChild(circle, label);
-	gameContainer.addChild(dragger);
-
-	dragger.on("pressmove", function(evt) {
-		evt.currentTarget.x = evt.stageX;
-		evt.currentTarget.y = evt.stageY;
-		stage.update();
-	});
+//	var dragger = new createjs.Container();
+//	dragger.x = dragger.y = 100;
+//	dragger.addChild(circle, label);
+//	gameContainer.addChild(dragger);
+//
+//	dragger.on("pressmove", function(evt) {
+//		evt.currentTarget.x = evt.stageX;
+//		evt.currentTarget.y = evt.stageY;
+//		stage.update();
+//	});
 
 	stage.addChild(gameContainer);
 	stage.update();
@@ -32,16 +32,21 @@ function createGame() {
 		switch (event.keyCode) {
 			case KEYCODE_UP:
 				circle.y -= 5;
+				label.y -= 5;
 				break;
 			case KEYCODE_DOWN:
 				circle.y += 5;
+				label.y += 5;
 				break;
 			case KEYCODE_LEFT:
 				circle.x -= 5;
+				label.x -= 5;
 				break;
 			case KEYCODE_RIGHT:
 				circle.x += 5;
+				label.x += 5;
 				break;
-  		}
+		}
+		stage.update();
 	});
 }
