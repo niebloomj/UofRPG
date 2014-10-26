06955e89d1a53843ba06fb1e7f1cc0695f977cfa
niebloomj <niebloomj@gmail.com>
2014-10-25 22:49:03 -0400

    Separated Container + Comments

--- webRoot/js/game.js
+++ webRoot/js/game.js
@@ -4,17 +4,26 @@ var inMotion = false,
 	circle;
 
 function createGame() {
+	//Create Game Container
 	gameContainer = new createjs.Container();
 	gameContainer.mouseMoveOutside = true;
-
+	//Create Map
 	createMap(playerX, playerY, 0, 0);
-
-	circle = new createjs.Shape();
-	circle.graphics.beginFill("red").drawCircle(496, 240, 16);
-	gameContainer.addChild(circle);
+	//Add Game Container to the Stage
 	stage.addChild(gameContainer);
 	stage.update();
-
+	//Create Player Container
+	playerContainer = new createjs.Container();
+	playerContainer.mouseMoveOutside = true;
+	//Create red circle- this will be the character
+	circle = new createjs.Shape();
+	circle.graphics.beginFill("red").drawCircle(496, 240, 16);
+	//Add the red circle to the player container
+	playerContainer.addChild(circle);
+	//Add the player container to the stage
+	stage.addChild(playerContainer);
+	stage.update();
+	//Add the Keydown Listener
 	document.addEventListener("keydown", function(event) {
 		keydown(event);
 	});
