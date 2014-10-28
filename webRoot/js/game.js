7d333854f330e31bdf31435082d943a62672595f
niebloomj <niebloomj@gmail.com>
2014-10-28 16:08:22 -0400

    Reimplemented Keydown.js

--- webRoot/js/game.js
+++ webRoot/js/game.js
@@ -1,68 +1,35 @@
 var inMotion = false,
-	playerX = 3200,
-	playerY = 3200,
-	deltaX = 0,
-	deltaY = 0,
-	circle,
-	KEYCODE_LEFT = 37,
-	KEYCODE_RIGHT = 39,
-	KEYCODE_UP = 38,
-	KEYCODE_DOWN = 40;
+    playerX = 3200,
+    playerY = 3200,
+    deltaX = 0,
+    deltaY = 0,
+    circle,
+    KEYCODE_LEFT = 37,
+    KEYCODE_RIGHT = 39,
+    KEYCODE_UP = 38,
+    KEYCODE_DOWN = 40;
 
 function createGame() {
-	//Create Game Container
-	gameContainer = new createjs.Container();
-	gameContainer.mouseMoveOutside = true;
+    //Create Game Container
+    gameContainer = new createjs.Container();
+    gameContainer.mouseMoveOutside = true;
 
-	stage.addChild(gameContainer);
-			
-	document.addEventListener("keydown", function(event) {
-		switch (event.keyCode) {
-			case KEYCODE_UP:
-				inMotion=true;
-				deltaY=-5;
-				break;
-			case KEYCODE_DOWN:
-				inMotion=true;
-				deltaY=5;
-				break;
-			case KEYCODE_LEFT:
-				inMotion=true;
-				deltaX=-5;
-				break;
-			case KEYCODE_RIGHT:
-				inMotion=true;
-				deltaX=5;
-				break;
-		}
-	});
+    stage.addChild(gameContainer);
 
-	document.addEventListener("keyup", function(event) {
-		switch (event.keyCode) {
-			case KEYCODE_UP:
-				inMotion=false;
-				deltaY=0;
-				break;
-			case KEYCODE_DOWN:
-				inMotion=false;
-				deltaY=0;
-				break;
-			case KEYCODE_LEFT:
-				inMotion=false;
-				deltaX=0;
-				break;
-			case KEYCODE_RIGHT:
-				inMotion=false;
-				deltaX=0;
-				break;
-		}
-	});
+    document.addEventListener("keydown", function(event) {
+        keydown(event);
+    });
 
-	createjs.Ticker.on("tick", tick);
-	createjs.Ticker.setFPS(30);
+    document.addEventListener("keyup", function(event) {
+        keyup(event);
+    });
+
+    createjs.Ticker.on("tick", tick);
+    createjs.Ticker.setFPS(30);
 }
+
 function tick(event) {
-	playerX+=deltaX;
-	playerY+=deltaY;
-	createMap(playerX,playerY);
+    playerX += deltaX;
+    playerY += deltaY;
+    createMap(playerX, playerY);
 }
--- webRoot/js/keydown.js
+++ webRoot/js/keydown.js
@@ -1,56 +1,42 @@
-var n = false,
-	playerX = 100,
-	playerY = 100,
-	KEYCODE_LEFT = 37,
-	KEYCODE_RIGHT = 39,
-	KEYCODE_UP = 38,
-	KEYCODE_DOWN = 40;
-
 function keydown(event) {
-	var i,
-		dist = 32,
-		timeToMove = 192;
 
-	if (!inMotion) {
-		switch (event.keyCode) {
-			case KEYCODE_UP:
-				inMotion=true;
-				deltaY+=5;
-				break;
-			case KEYCODE_DOWN:
-				inMotion=true;
-				deltaY-=5;
-				break;
-			case KEYCODE_LEFT:
-				inMotion=true;
-				deltaX-=5;
-				break;
-			case KEYCODE_RIGHT:
-				inMotion=true;
-				deltaX+=5;
-				break;
-		}
-	}
-	stage.update();
+    switch (event.keyCode) {
+        case KEYCODE_UP:
+            inMotion = true;
+            deltaY = -5;
+            break;
+        case KEYCODE_DOWN:
+            inMotion = true;
+            deltaY = 5;
+            break;
+        case KEYCODE_LEFT:
+            inMotion = true;
+            deltaX = -5;
+            break;
+        case KEYCODE_RIGHT:
+            inMotion = true;
+            deltaX = 5;
+            break;
+    }
 }
 
-function keyup(event){
-		switch (event.keyCode) {
-			case KEYCODE_UP:
-				inMotion=false;
-				deltaY-=5;
-				break;
-			case KEYCODE_DOWN:
-				inMotion=false;
-				deltaY+=5;
-				break;
-			case KEYCODE_LEFT:
-				inMotion=false;
-				deltaX+=5;
-				break;
-			case KEYCODE_RIGHT:
-				inMotion=false;
-				deltaX-=5;
-				break;
-		}
+function keyup(event) {
+    switch (event.keyCode) {
+        case KEYCODE_UP:
+            inMotion = false;
+            deltaY = 0;
+            break;
+        case KEYCODE_DOWN:
+            inMotion = false;
+            deltaY = 0;
+            break;
+        case KEYCODE_LEFT:
+            inMotion = false;
+            deltaX = 0;
+            break;
+        case KEYCODE_RIGHT:
+            inMotion = false;
+            deltaX = 0;
+            break;
+    }
 }
