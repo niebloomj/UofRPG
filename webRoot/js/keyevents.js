7ffeae754d480998be5e0a96d49ce5849a7f8e6b
Hayden Schiff <oxguy3@gmail.com>
2014-11-11 16:10:22 -0500

    added keybinds: WASD to move, '\' for benchmarking

--- webRoot/js/keyevents.js
+++ webRoot/js/keyevents.js
@@ -1,17 +1,37 @@
+// key bindings
+var KEYCODE_DEBUG = 192; // grave (`)
+var KEYCODE_BENCHMARK = 220; // backslash (\)
+
+var KEYCODE_LEFT = 37; // left arrow
+var KEYCODE_RIGHT = 39; // right arrow
+var KEYCODE_UP = 38; // up arrow
+var KEYCODE_DOWN = 40; // down arrow
+
+var KEYCODE_LEFT_ALT = 65; // A
+var KEYCODE_RIGHT_ALT = 68; // D
+var KEYCODE_UP_ALT = 87; // W
+var KEYCODE_DOWN_ALT = 83; // S
+
+var KEYCODE_TOGGLE_PLAYER = 80; // P
+
 function keydown(event) {
     player.isSprinting = event.shiftKey;
     player.isNoCollide = event.ctrlKey;
     switch (event.keyCode) {
         case KEYCODE_UP:
+        case KEYCODE_UP_ALT:
             player.isMoveU = true;
             break;
         case KEYCODE_DOWN:
+        case KEYCODE_DOWN_ALT:
             player.isMoveD = true;
             break;
         case KEYCODE_LEFT:
+        case KEYCODE_LEFT_ALT:
             player.isMoveL = true;
             break;
         case KEYCODE_RIGHT:
+        case KEYCODE_RIGHT_ALT:
             player.isMoveR = true;
             break;
     }
@@ -22,19 +42,26 @@ function keyup(event) {
     player.isNoCollide = event.ctrlKey;
     switch (event.keyCode) {
         case KEYCODE_UP:
+        case KEYCODE_UP_ALT:
             player.isMoveU = false;
             break;
         case KEYCODE_DOWN:
+        case KEYCODE_DOWN_ALT:
             player.isMoveD = false;
             break;
         case KEYCODE_LEFT:
+        case KEYCODE_LEFT_ALT:
             player.isMoveL = false;
             break;
         case KEYCODE_RIGHT:
+        case KEYCODE_RIGHT_ALT:
             player.isMoveR = false;
             break;
         case KEYCODE_DEBUG:
             debugMode = !debugMode;
+            break;
+        case KEYCODE_BENCHMARK:
+            benchmarkingMode = !benchmarkingMode;
             break;
         case KEYCODE_TOGGLE_PLAYER:
             player.currentCharacter = (player.currentCharacter + 1); //% player.characters.length;
