332518675c30d533a61abf513b3b451670a3120c
AaronMcClure <aaron.t.mcclure@gmail.com>
2014-11-20 19:26:40 -0500

    Collider class for entities. Implemented w/ Coin.
    Conflicts:
    	webRoot/js/uros.class.js

--- webRoot/js/uros.class.js
+++ webRoot/js/uros.class.js
@@ -17,7 +17,11 @@ Uros.prototype.tick = function(delta) {
 	var xDist = this.x - player.x;
 	var yDist = this.y - player.y;
 	var distance = Math.sqrt(xDist * xDist + yDist * yDist);
-	if (distance < 20) {
+	
+	//console.log("player x " + player.x);
+	var isCollision = (new Collider(this.x, this.y, this.width, this.height).contains(player.x, player.y, player.width, player.height));
+	
+	if (isCollision){//distance < 20) {
 		var indexOfUro = entities.indexOf(this);
 		if (indexOfUro > -1) {
 			entities.splice(indexOfUro, 1);
@@ -60,13 +64,17 @@ Uros.prototype.move = function(delta) {
 };
 
 Uros.prototype.getDisplay = function() {
+<<<<<<< HEAD
 	var sprite = new createjs.Bitmap("img/coin.png");
-	sprite.setTransform(0,16);
+	//sprite.setTransform(0,16);
 
 	return sprite;
 	/*var circle = new createjs.Shape();
+=======
+	var circle = new createjs.Shape();
+>>>>>>> origin/master
 	circle.graphics.beginFill("yellow").drawCircle(0, 0, 16);
-	return circle;*/
+	return circle;
 };
 
 Uros.prototype.width = function() {
