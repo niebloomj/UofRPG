ea04a9df8d81ca82015f9d70dff226537c6a189f
Brad Beyers <beyers.bradley@gmail.com>
2014-11-23 21:03:08 -0500

    MORE FUNCTIONS
    i actually removed some stuff to make it easier to generalize in the
    future

--- webRoot/js/init.js
+++ webRoot/js/init.js
@@ -96,7 +96,7 @@ $.getScript("js/keyevents.js", function() {
                             $.getScript("js/combat.js", function() {
                                 $.getScript("js/collider.class.js", function() {
                                     $.getScript("js/Entities/healthblobs.class.js", function() {
-                                        $.getScript("js/enemy.class.js", function() {
+                                        $.getScript("js/Enemies/enemy.class.js", function() {
 
                                         });
                                     });
