c7362a1a23c961ec9e3ed19afeef5186e2e143de
Brad Beyers <beyers.bradley@gmail.com>
2014-11-23 22:57:19 -0500

    you can't bandeau the rando

--- webRoot/js/Enemies/rando.class.js
+++ webRoot/js/Enemies/rando.class.js
@@ -1,7 +1,50 @@
-Rando.prototype = new Enemy("Rando", 1, 100, 10, 10, 10);
+Rando.prototype = new Enemy("Rando", 1, 25, 20, 10, 5);
+
+Rando.prototype.decide = function() {
+	if (this.health / this.maxHealth > .75) {
+		this.roll();
+		if (this.rng > 80) {
+			this.attack();
+		} else {
+			this.defend();
+		}
+	} else if (this.health / this.maxHealth > .50) {
+		this.roll();
+		if (this.rng > 70) {
+			this.attack();
+		} else {
+			this.defend();
+		}
+	} else if (this.health / this.maxHealth > .25) {
+		this.roll();
+		if (this.rng > 60) {
+			this.attack();
+		} else if (this.rng > 30) {
+			this.defend();
+		} else {
+			this.healSelf();
+		}
+	} else {
+		this.roll();
+		if (this.rng > 90) {
+			this.castspell(); //Desperation attack!!
+		} else if (this.rng > 70) {
+			this.attack();
+		} else if (this.rng > 40) {
+			this.defend();
+		} else {
+			this.healSelf();
+		}
+	}
+};
+
+Rando.prototype.healSelf = function() {
+	this.heal(10);
+}
 
 Rando.prototype.attack = function() {
-	if (randomInt(0, 100) > 25) {
+	this.roll();
+	if (this.rng > 25) {
 		player.takeDamage(this.strength);
 	} else {
 		//Rando misses :(
@@ -12,7 +55,11 @@ Rando.prototype.defend = function() {
 	this.defense += this.defense / 2; //Randos are pretty good at defending. Who knew?
 }
 
-Rando.prototype.castspell = function(spellDamage) {
-	//Randos use fireballs, by the way
-	
-}
+Rando.prototype.castspell = function() {
+	this.roll();
+	if (this.rng > 75) {
+		player.takeDamage(25);
+	} else {
+		//rando's spell fails
+	}
+};
