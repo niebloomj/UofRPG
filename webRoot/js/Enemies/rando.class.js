a99988160c9b2ee3a18755cd7c8d84d163ddcb3c
niebloomj <niebloomj@gmail.com>
2014-11-23 22:11:37 -0500

    Revert "go"
    This reverts commit 75cad6ed15e09a7fb6802fc2352211eee03df66b, reversing
    changes made to e92d09224ddb0f97d42d2bcd14ee6a6313564fda.

--- webRoot/js/Enemies/rando.class.js
+++ webRoot/js/Enemies/rando.class.js
@@ -1,65 +1,18 @@
-Rando.prototype = new Enemy("Rando", 1, 25, 20, 10, 5);
-
-Rando.prototype.decide = function() {
-	if (this.health / this.maxHealth > .75) {
-		this.roll();
-		if (this.rng > 80) {
-			this.attack();
-		} else {
-			this.defend();
-		}
-	} else if (this.health / this.maxHealth > .50) {
-		this.roll();
-		if (this.rng > 70) {
-			this.attack();
-		} else {
-			this.defend();
-		}
-	} else if (this.health / this.maxHealth > .25) {
-		this.roll();
-		if (this.rng > 60) {
-			this.attack();
-		} else if (this.rng > 30) {
-			this.defend();
-		} else {
-			this.healSelf();
-		}
-	} else {
-		this.roll();
-		if (this.rng > 90) {
-			this.castspell(); //Desperation attack!!
-		} else if (this.rng > 70) {
-			this.attack();
-		} else if (this.rng > 40) {
-			this.defend();
-		} else {
-			this.healSelf();
-		}
-	}
-};
-
-Rando.prototype.healSelf = function() {
-	this.heal(10);
-}
+Rando.prototype = new Enemy("Rando", 1, 100, 10, 10, 10);
 
 Rando.prototype.attack = function() {
-	this.roll();
-	if (this.rng > 25) {
+	if (randomInt(0, 100) > 25) {
 		player.takeDamage(this.strength);
 	} else {
 		//Rando misses :(
 	}
-};
+}
 
 Rando.prototype.defend = function() {
 	this.defense += this.defense / 2; //Randos are pretty good at defending. Who knew?
-};
+}
 
-Rando.prototype.castspell = function() {
-	this.roll();
-	if (this.rng > 75) {
-		player.takeDamage(25);
-	} else {
-		//rando's spell fails
-	}
-};
+Rando.prototype.castspell = function(spellDamage) {
+	//Randos use fireballs, by the way
+	
+}
