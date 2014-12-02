a75aa910f25def6dfc8feacb09f18848cc41fa54
AaronMcClure <aaron.t.mcclure@gmail.com>
2014-12-01 19:27:48 -0500

    Revert "Rando"
    This reverts commit e1509f94b7a831b7f048eec01dba4b211545f8f8.

--- webRoot/js/Enemies/rando.class.js
+++ webRoot/js/Enemies/rando.class.js
@@ -1,5 +1,4 @@
-//var Rando = function(){};
-function Rando(){};
+var Rando = function(){};
 
 Rando.prototype = new Enemy("Rando", 1, 25, 20, 10, 5);
 
@@ -7,43 +6,42 @@ Rando.prototype.decide = function() {
 	if (this.health / this.maxHealth > .75) {
 		this.roll();
 		if (this.rng > 80) {
-			return this.attack();
+			this.attack();
 		} else {
-			return this.defend();
+			this.defend();
 		}
 	} else if (this.health / this.maxHealth > .50) {
 		this.roll();
 		if (this.rng > 70) {
-			return this.attack();
+			this.attack();
 		} else {
-			return this.defend();
+			this.defend();
 		}
 	} else if (this.health / this.maxHealth > .25) {
 		this.roll();
 		if (this.rng > 60) {
-			return this.attack();
+			this.attack();
 		} else if (this.rng > 30) {
-			return this.defend();
+			this.defend();
 		} else {
-			return this.healSelf();
+			this.healSelf();
 		}
 	} else {
 		this.roll();
 		if (this.rng > 90) {
-			return this.castspell(); //Desperation attack!!
+			this.castspell(); //Desperation attack!!
 		} else if (this.rng > 70) {
-			return this.attack();
+			this.attack();
 		} else if (this.rng > 40) {
-			return this.defend();
+			this.defend();
 		} else {
-			return this.healSelf();
+			this.healSelf();
 		}
 	}
 };
 
 Rando.prototype.healSelf = function() {
 	this.heal(10);
-	return "heal 10";
 };
 
 Rando.prototype.attack = function() {
@@ -52,36 +50,28 @@ Rando.prototype.attack = function() {
 		this.roll();
 		if (this.rng > 25) {
 			player.takeDamage(this.strength);
-			return "attack " + this.strength;
 		} else {
 			//Rando misses :(
-			return "attack 0";
 		}
 	} else {
 		this.roll();
 		if (this.rng > 50) {
 			player.takeDamage(this.strength * 1.5);
-			return "attack " + (this.strength*1.5);
 		} else {
 			//Rando misses :(
-			return "attack 0";
 		}
 	}
 };
 
-// what?
 Rando.prototype.defend = function() {
-	this.defense += this.defense / 2;
-	return "defend 0"; 
+	this.defense += this.defense / 2; //Randos are pretty good at defending. Who knew?
 };
 
 Rando.prototype.castspell = function() {
 	this.roll();
 	if (this.rng > 75) {
 		player.takeDamage(25);
-		return "spell 25";
 	} else {
 		//rando's spell fails
-		return "spell 0";
 	}
 };
