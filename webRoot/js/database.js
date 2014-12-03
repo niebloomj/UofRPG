eb70e7ca4ec84fb6f170d5c1840ed9e0dbd1450c
niebloomj <niebloomj@gmail.com>
2014-12-02 22:44:52 -0500

    1.5 Second time between saving

--- webRoot/js/database.js
+++ webRoot/js/database.js
@@ -1,43 +1,49 @@
+var lastSave = 0;
+
 function saveGame() {
-    console.log("Saving Game");
-    var query = new Parse.Query(PlayerStatsTable);
-    query.equalTo("Username", username);
-    query.find({
-        success: function(results) {
-            if (results.length == 1) {
-                var object = results[0];
-                object.save(null, {
-                    success: function(object) {
-                        object.set("Steps", player.totalMoved);
-                        object.set("PlayerX", player.x);
-                        object.set("PlayerY", player.y);
-                        object.set("Health", player.health);
-                        object.set("Uros", player.money);
-                        object.save();
-                    }
-                });
-            } else if (results.length == 0) {
-                var statTable = new PlayerStatsTable()
-                statTable.set("Username", username);
-                statTable.save(null, {
-                    success: function(statTable) {
-                        object.set("Steps", player.totalMoved);
-                        object.set("PlayerX", player.x);
-                        object.set("PlayerY", player.y);
-                        object.set("Health", player.health);
-                        object.set("Uros", player.money);
-                        object.save();
-                    }
-                });
-            } else {
-                //Well this is actually a HUGE PROBLEM if we reach this else
-                alert("ERROR ERROR ERROR!!!");
+    var nowSave = new Date().getTime();
+    if (nowSave - lastSave > 1500 || lastSave == 0) {
+        lastSave = new Date().getTime();
+        console.log("Saving Game");
+        var query = new Parse.Query(PlayerStatsTable);
+        query.equalTo("Username", username);
+        query.find({
+            success: function(results) {
+                if (results.length == 1) {
+                    var object = results[0];
+                    object.save(null, {
+                        success: function(object) {
+                            object.set("Steps", player.totalMoved);
+                            object.set("PlayerX", player.x);
+                            object.set("PlayerY", player.y);
+                            object.set("Health", player.health);
+                            object.set("Uros", player.money);
+                            object.save();
+                        }
+                    });
+                } else if (results.length == 0) {
+                    var statTable = new PlayerStatsTable()
+                    statTable.set("Username", username);
+                    statTable.save(null, {
+                        success: function(statTable) {
+                            object.set("Steps", player.totalMoved);
+                            object.set("PlayerX", player.x);
+                            object.set("PlayerY", player.y);
+                            object.set("Health", player.health);
+                            object.set("Uros", player.money);
+                            object.save();
+                        }
+                    });
+                } else {
+                    //Well this is actually a HUGE PROBLEM if we reach this else
+                    alert("ERROR ERROR ERROR!!!");
+                }
+            },
+            error: function(error) {
+                showLoginMessage(error.message, "danger");
             }
-        },
-        error: function(error) {
-            showLoginMessage(error.message, "danger");
-        }
-    });
+        });
+    }
 }
 
 function loadSavedGame() {
